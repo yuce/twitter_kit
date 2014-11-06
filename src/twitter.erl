9032da41163c37956ed55adae294b4369d4ed168
Yuce Tekol <yucetekol@gmail.com>
2014-11-06 11:13:20 +0200

    Removed unnecessary twitter:new/2

--- src/twitter.erl
+++ src/twitter.erl
@@ -10,35 +10,24 @@
 -author("Yuce").
 
 %% API
--export([new/1, new/2, get/3]).
+-export([new/1, get/3]).
 
 -include("oauth.hrl").
 -include("twitter.hrl").
 
 -define(ifte(Cond, True, False), (if Cond -> True; true -> False end)).
 
-
 -spec new(#oauth{}) -> #twitter{}.
 
-%% @doc Create new twitter record with optional overrides.
-
-new(Auth) ->
-  new(Auth, []).
-
--spec new(#oauth{}, [param()]) -> #twitter{}.
--type param()          :: {param_key(), param_value()}.
--type param_key()      :: atom().
--type param_value()    :: string().
-
-new(Auth, Options) when Auth =/= nil ->
-  get_twitter_opts(#twitter{auth = Auth}, Options).
+%% @doc Create new twitter record.
+new(Auth) when Auth =/= nil ->
+  #twitter{auth = Auth}.
 
 -spec get(#twitter{}, path(), [param()]) -> reply().
 -type path() :: atom() | string().
 -type reply() :: {ok, list()} | {error, integer()} | {error, any()}.
 
 %% @doc Access Twitter endpoint with Path and Arguments.
-
 get(Twitter, Path, Args) ->
     Uri = make_uri(Twitter, Path, Args),
     case httpc:request(get, {Uri, []}, [], [{body_format, binary}]) of
@@ -52,6 +41,9 @@ get(Twitter, Path, Args) ->
     end.
 
 -spec make_uri(#twitter{}, path(), [param()]) -> string().
+-type param()          :: {param_key(), param_value()}.
+-type param_key()      :: atom().
+-type param_value()    :: string().
 
 make_uri(#twitter{auth=Auth, domain=Domain, api_version=ApiVersion,
             secure=Secure, format=Format}, Path, Args) ->
@@ -61,22 +53,3 @@ make_uri(#twitter{auth=Auth, domain=Domain, api_version=ApiVersion,
     EncodedParams = oauth:encode_params(Auth, BaseUrl, "GET", Args),
     string:join([BaseUrl, EncodedParams], "?").
 
--spec get_twitter_opts(#twitter{}, [param()]) -> #twitter{}.
-
-get_twitter_opts(Twitter, []) ->
-  Twitter;
-
-get_twitter_opts(Twitter, [H|T]) ->
-  NewTwitter = case H of
-    {domain, Domain} ->
-      Twitter#twitter{domain = Domain};
-    {format, Format} ->
-      Twitter#twitter{format = Format};
-    {secure, Secure} ->
-      Twitter#twitter{secure = (Secure == true)};
-    {api_version, ApiVersion} ->
-      Twitter#twitter{api_version = ApiVersion};
-     _ ->
-       Twitter
-  end,
-  get_twitter_opts(NewTwitter, T).
