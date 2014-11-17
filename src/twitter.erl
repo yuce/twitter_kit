4dd8497447b188145865e925506b88b088b8d5a5
Yuce Tekol <yucetekol@gmail.com>
2014-11-17 05:32:44 +0200

    Cleanup

--- src/twitter.erl
+++ src/twitter.erl
@@ -1,7 +1,7 @@
 -module(twitter).
 -author("Yuce Tekol").
 
--export([new/1, new/2]).
+-export([new/1]).
 
 -include("twitter.hrl").
 -include("def.hrl").
@@ -10,35 +10,5 @@
 -spec new(#oauth{}) -> #twitter{}.
 
 new(Auth) ->
-     new(Auth, []).
+     #twitter{auth = Auth}.
 
-
--spec new(#oauth{}, twitter_options()) -> #twitter{}.
-
-new(Auth, Options) ->
-    make_twitter(#twitter{auth = Auth}, Options).
-
-
--spec make_twitter(#twitter{}, twitter_options()) -> #twitter{}.
-
-make_twitter(Api, []) ->
-    Api;
-
-make_twitter(Api, [{Name, Value}|T]) ->
-    case Name of
-        json_decode ->
-            make_twitter(Api#twitter{json_decode = Value}, T) end.
-
-
-%% Internal Tests
-
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
-
-new_test() ->
-    Fun = fun(_) -> [] end,
-    Api = new(#oauth{}, [{json_decode, Fun}]),
-    #twitter{json_decode=JsonDecode} = Api,
-    ?assertEqual(JsonDecode, Fun).
-
--endif.
